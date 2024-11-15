f13793752201db3313727d4928c89069a9b41961
Apil Neupane <apilneupane123@gmail.com>
2024-11-15 14:06:46 +0545

    Work in Process

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
+import { Fillup } from './Fillup'
 
 export function Lists() {
   return (
-    <div className='container mx-auto'>
-        <div className='New_Task'>
-            <h1 className=''> New Task</h1>
-            <input type="text" placeholder='Enter a new task' className='border border-black pt-2 pb-2 pl-2 pr-40'/>
-            <button>Add Task</button>
-        </div>
-        <div className='Active Tasks'>
-
-        </div>
+    <div className='container mx-auto bg-blue-300 rounded-3xl'>
+        <Fillup/>
     </div>
   )
 }
